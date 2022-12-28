83496f88583fc5d350528e474e54b598c364e15e
0412shsh <0412corn@naver.com>
2022-12-28 21:08:17 +0900

    [add] 타임리프 레이아웃 생성
    * templates, static 폴더 안에 각각 html, css 파일 생성

---
+++ src/main/java/com/pongcoder/myhome/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.pongcoder.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "index"; //index.html 로 이동
+    }
+}
